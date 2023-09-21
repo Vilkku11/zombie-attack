6c4548c8a6f76ba53bb6d5a8a6b7b74d3d7a4721
William Reima <william.reima@gmail.com>
2023-09-21 21:45:49 +0300

    initial commit

---
+++ Player.gd
@@ -0,0 +1,115 @@
+extends CharacterBody3D
+
+@export var inventory_data: InventoryData
+
+signal toggle_inventory()
+signal health_changed(health_value)
+
+@onready var camera = $Camera3D
+@onready var animation_player = $AnimationPlayer
+@onready var muzzle_flash = $Camera3D/Pistol/MuzzleFlash
+@onready var raycast = $Camera3D/RayCast3D
+@onready var interact_ray = $Camera3D/InteractRay
+
+
+var health = 3
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _enter_tree():
+	set_multiplayer_authority(str(name).to_int())
+	
+
+
+func _ready():
+	
+	if not is_multiplayer_authority(): return
+	
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	camera.current = true
+func _unhandled_input(event):
+	if not is_multiplayer_authority(): return
+	
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * 0.005)
+		camera.rotate_x(-event.relative.y * 0.005)
+		camera.rotation.x = clamp(camera.rotation.x, -PI/2, PI/2)
+	
+	if Input.is_action_just_pressed("shoot") and animation_player.current_animation != "shoot":
+		play_shoot_effects.rpc()
+		if raycast.is_colliding():
+			var hit_player = raycast.get_collider()
+			hit_player.receive_damage.rpc_id(hit_player.get_multiplayer_authority())
+			
+	if Input.is_action_just_pressed("inventory"):
+		toggle_inventory.emit()
+		
+	if Input.is_action_just_pressed("interact"):
+		interact()
+	
+func _physics_process(delta):
+	if not is_multiplayer_authority(): return
+	
+	#print(velocity)
+	
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("left", "right", "up", "down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+		
+		
+	if animation_player.current_animation == "shoot":
+		pass
+	elif input_dir != Vector2.ZERO and is_on_floor():
+		animation_player.play("move")
+	else:
+		animation_player.play("idle")
+	move_and_slide()
+
+func interact() -> void:
+	if interact_ray.is_colliding():
+		interact_ray.get_collider().player_interact()
+
+
+
+@rpc("call_local")
+func play_shoot_effects():
+	animation_player.stop()
+	animation_player.play("shoot")
+	muzzle_flash.restart()
+	muzzle_flash.emitting = true
+
+@rpc("any_peer")
+func receive_damage():
+	health -= 1
+	if health <= 0:
+		health = 3
+		position = Vector3.ZERO
+	health_changed.emit(health)
+
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "shoot":
+		animation_player.play("idle")
+
+
+
---
+++ world.gd
@@ -0,0 +1,100 @@
+extends Node
+
+@onready var main_menu = $CanvasLayer/MainMenu
+@onready var address_entry = $CanvasLayer/MainMenu/MarginContainer/VBoxContainer/AddressEntry
+@onready var hud = $CanvasLayer/HUD
+@onready var health_bar = $CanvasLayer/HUD/HealthBar
+
+@onready var inventory_interface = $CanvasLayer/HUD/InventoryInterface
+
+
+const Player = preload("res://player.tscn")
+const PORT = 9999
+var enet_peer = ENetMultiplayerPeer.new()
+
+
+func _unhandled_input(event):
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+		
+
+
+
+func _on_host_button_pressed():
+	hud.show()
+	main_menu.hide()
+	enet_peer.create_server(PORT)
+	multiplayer.multiplayer_peer = enet_peer
+	multiplayer.peer_connected.connect(add_player)
+	multiplayer.peer_disconnected.connect(remove_player)
+	
+	
+	
+	add_player(multiplayer.get_unique_id())
+	#upnp_setup()
+
+
+
+func _on_join_button_pressed():
+	hud.show()
+	main_menu.hide()
+	enet_peer.create_client("localhost", PORT)
+	multiplayer.multiplayer_peer = enet_peer
+	
+	
+	
+
+func add_player(peer_id):
+	var player = Player.instantiate()
+	
+	player.toggle_inventory.connect(toggle_inventory_interface)
+	inventory_interface.set_player_inventory_data(player.inventory_data)
+	
+	for node in get_tree().get_nodes_in_group("external_inventory"):
+		node.toggle_inventory.connect(toggle_inventory_interface)
+	
+	player.name = str(peer_id)
+	add_child(player)
+	if player.is_multiplayer_authority():
+		player.health_changed.connect(update_health_bar)
+		
+		
+		
+func toggle_inventory_interface(external_inventory_owner = null) -> void:
+	inventory_interface.visible = not inventory_interface.visible
+	
+	if inventory_interface.visible:
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	else:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+	if external_inventory_owner and inventory_interface.visible:
+		inventory_interface.set_external_inventory(external_inventory_owner)
+	else:
+		inventory_interface.clear_external_inventory()
+	
+func remove_player(peer_id):
+	var player = get_node_or_null(str(peer_id))
+	if player:
+		player.queue_free()
+	
+func update_health_bar(health_value):
+	health_bar.value = health_value
+	
+
+
+func _on_multiplayer_spawner_spawned(node):
+	if node.is_multiplayer_authority():
+		node.health_changed.connect(update_health_bar)
+
+func upnp_setup():
+	var upnp = UPNP.new()
+	var discover_result = upnp.discover()
+	assert(discover_result == UPNP.UPNP_RESULT_SUCCESS, "UPNP Discover Failed! Error %s" % discover_result)
+	
+	assert(upnp.get_gateway() and upnp.get_gateway().is_valid_gateway(), "UPNP Invalid Gateway!")
+
+	var map_result = upnp.add_port_mapping(PORT)
+	assert(map_result == UPNP.UPNP_RESULT_SUCCESS, "UPNP Port Mapping Failed! Error %s" % map_result)
+	
+	print("Success! Join Address: %s" % upnp.query_external_address())
